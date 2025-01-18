ca0b5e7d8b34bb09f0be423fe5d762a099cfd845
Mélina POUVRASSEAU <github_mg4wr@pouvrasseau.com>
2025-01-18 16:15:10 +0100

    exercice 1 fni

---
+++ lesson_01/script.py
@@ -0,0 +1,36 @@
+n1= 15
+n2= 30
+print ((n1+n2)/2)
+
+h1=10
+h2=65
+print (h1+h2)
+print (h1-h2)
+print (h1*h2)
+print (h1/h2)
+print (h2 % h1)
+
+name01= 'melina'
+print(name01)
+
+f1, f2, f3= 'patate','salade','carotte'
+print (f1+', '+f2+', '+f3)
+
+print("hello"*10)
+
+name, age="melina", 17
+print ('my name is',name,'and i am',age,'years old')
+
+prénom= 'melina'
+nom= 'pouvrasseau'
+nomcomplet= prénom +' '+ nom
+print (nomcomplet)
+
+repas1= 'des coquilletes avec du jambon'
+print (repas1[4])
+
+repas2= 'des coquilletes avec du jambon'
+print (repas2[0:5])
+
+repas3= 'des coquilletes'
+print (repas3[0:15])
